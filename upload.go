4b5a48bb1f5892c12e4152729dd91bc8cfbbbcde
Radu Potop <radu@wooptoo.com>
2024-03-27 23:31:56 +0000

    Split app into multiple packages

--- upload.go
+++ upload.go
@@ -4,17 +4,10 @@ import (
 	"fmt"
 	"html/template"
 	"io"
-	"mime/multipart"
 	"net/http"
 	"os"
 	"path/filepath"
 	"slices"
-
-	"github.com/aws/aws-sdk-go/aws"
-	"github.com/aws/aws-sdk-go/aws/credentials"
-	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/s3"
-	"github.com/subosito/gotenv"
 )
 
 const (
@@ -23,10 +16,6 @@ const (
 )
 
 var (
-	awsRegion          = os.Getenv("AWS_REGION")
-	awsAccessKeyID     = os.Getenv("AWS_KEY_ID")
-	awsSecretKey       = os.Getenv("AWS_SECRET")
-	s3Bucket           = os.Getenv("AWS_BUCKET")
 	ALLOWED_EXTENSIONS = []string{".png", ".jpg", ".jpeg"}
 )
 
@@ -52,34 +41,6 @@ const formHTML = `
 func formHandler(w http.ResponseWriter, r *http.Request) {
 	tmpl := template.Must(template.New("form").Parse(formHTML))
 	tmpl.Execute(w, nil)
-}
-
-// Upload to S3 bucket
-func putToS3(w http.ResponseWriter, file multipart.File, handler *multipart.FileHeader, prevMsg string) {
-	sess, err := session.NewSession(&aws.Config{
-		Region:      aws.String(awsRegion),
-		Credentials: credentials.NewStaticCredentials(awsAccessKeyID, awsSecretKey, ""),
-	})
-	if err != nil {
-		http.Error(w, "Failed to create AWS session", http.StatusInternalServerError)
-		return
-	}
-
-	svc := s3.New(sess)
-
-	// Upload file to S3 bucket
-	_, err = svc.PutObject(&s3.PutObjectInput{
-		Bucket: aws.String(s3Bucket),
-		Key:    aws.String(handler.Filename),
-		Body:   file,
-		ACL:    aws.String("public-read"),
-	})
-	if err != nil {
-		http.Error(w, fmt.Sprintf("%s\nFailed to upload file to S3 bucket", prevMsg), http.StatusInternalServerError)
-		return
-	}
-
-	fmt.Fprint(w, "File uploaded successfully!")
 }
 
 // Handler for uploading file to S3
@@ -138,12 +99,6 @@ func uploadHandler(w http.ResponseWriter, r *http.Request) {
 	}
 
 	fmt.Fprint(w, statusMsg)
-}
-
-// only called once
-func init() {
-	gotenv.Load()
-	// fmt.Println(os.Environ())
 }
 
 func main() {
